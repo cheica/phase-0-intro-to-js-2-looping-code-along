f9353b1c7f96fb5b6adb4378c40153fdd111bc28
Chelsea <chelsea.icaza10@hotmail.com>
2021-07-10 12:30:46 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+const newMssg = [];
+
+function writeCards(names,event){
+    for (let i= 0; i < names.length; i++) {
+       newMssg.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+
+       debugger;
+       
+       
+    }
+
+    return newMssg;
+}
+
+
+//countDown()
+function countDown(){
+let countDown = 10; 
+    while (countDown >= 0){
+        console.log(countDown--);
+    }
+
+    return countDown;
+}
